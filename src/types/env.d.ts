e26a3ecb9b28cdbb428d0a318513cf5f842a213b
rom6621 <3103rom@gmail.com>
2023-11-05 07:35:53 +0900

    fix: NextAuthのシークレット用の設定を作成

--- src/types/env.d.ts
+++ src/types/env.d.ts
@@ -1,5 +1,6 @@
 declare namespace NodeJS {
   interface ProcessEnv {
+    readonly NEXTAUTH_SECRET: string;
     readonly GOOGLE_CLIENT_ID: string;
     readonly GOOGLE_CLIENT_SECRET: string;
     readonly NEXT_PUBLIC_FETCH_HOST: string;
