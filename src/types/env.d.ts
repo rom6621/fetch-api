efde2381ec5339eef3acb01572d3b9df0cd69a26
rom6621 <3103rom@gmail.com>
2023-11-05 07:25:31 +0900

    feat: IdentityPlatformを使用した認証機能を削除

--- src/types/env.d.ts
+++ src/types/env.d.ts
@@ -3,7 +3,5 @@ declare namespace NodeJS {
     readonly GOOGLE_CLIENT_ID: string;
     readonly GOOGLE_CLIENT_SECRET: string;
     readonly NEXT_PUBLIC_FETCH_HOST: string;
-    readonly FIREBASE_API_KEY: string;
-    readonly FIREBASE_AUTH_DOMAIN: string;
   }
 }
