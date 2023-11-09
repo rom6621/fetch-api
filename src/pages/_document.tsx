b0fb2cad14de45e258bd22f25a829635d73a7064
rom6621 <3103rom@gmail.com>
2023-11-09 22:27:36 +0900

    feat: titleを追加

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from "next/document";
 const Document = () => {
   return (
     <Html lang="ja">
-      <Head />
+      <Head>
+        <title>Fetch API Sample</title>
+      </Head>
       <body>
         <Main />
         <NextScript />
