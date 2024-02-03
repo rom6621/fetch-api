d43d5ad80b53b0feed8f21846cfec3b135b1eaea
rom6621 <3103rom@gmail.com>
2024-02-04 01:30:14 +0900

    feat: Firebase Authに対応

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,27 @@
 import { TestForm } from "@/components/TestForm";
 import { useApi } from "@/hooks/useApi";
+import { auth } from "@/utils/firebase";
 import { Grid, GridItem } from "@chakra-ui/react";
+import { GoogleAuthProvider, signInWithCredential } from "firebase/auth";
 import { signIn, signOut, useSession } from "next-auth/react";
+import { useEffect, useState } from "react";
 
 const Home = () => {
   const { data: session, status } = useSession();
   const { result, error, fetchApi } = useApi();
+  const [firebaseIdToken, setFirebaseIdToken] = useState("");
+
+  const getIdToken = async (idToken: string) => {
+    const credential = GoogleAuthProvider.credential(idToken);
+    const { user } = await signInWithCredential(auth, credential);
+    setFirebaseIdToken(await user.getIdToken());
+  };
+
+  useEffect(() => {
+    if (session?.credential.idToken) {
+      getIdToken(session.credential.idToken);
+    }
+  }, [session]);
 
   return (
     <Grid py={4} gap={5} templateColumns="repeat(6, 1fr)">
@@ -13,8 +29,9 @@ const Home = () => {
         <TestForm
           isLoading={status === "loading"}
           isSignIn={!!session}
-          idToken={session?.credential.idToken}
-          accessToken={session?.credential.idToken}
+          firebaseIdToken={firebaseIdToken}
+          googleIdToken={session?.credential.idToken}
+          googleAccessToken={session?.credential.accessToken}
           result={result}
           error={error}
           signIn={async () => {
